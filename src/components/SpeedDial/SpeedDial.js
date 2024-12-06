e5a3aa0e8bfca25d7b8fd3a5e1fb52b42d55b05a
Torgbe-Desmond <123torgbe@gmail.com>
2024-12-06 04:37:05 -0800

    fixed some render bugs

--- src/components/SpeedDial/SpeedDial.js
+++ src/components/SpeedDial/SpeedDial.js
@@ -41,7 +41,7 @@ export default function BasicSpeedDial({
         { iconType: <DriveFileMoveIcon />, color: 'primary', disabled: selectedItems?.length === 0 || isValid, action: () => handleAction('Move'), label: 'Move' },
         { iconType: <FileDownloadIcon />, color: 'primary', disabled: selectedFilesForOptions?.length !== 1 || isValid, action: () => handleAction('Download'), label: 'Download' },
         { iconType: <FileUploadOutlinedIcon />, color: 'primary', disabled: selectedFilesForOptions?.length == 0 || isValid, action: () => handleAction('GenerateSecretCode'), label: 'Share Files' },
-        { iconType: <FileDownloadOutlinedIcon />, color: 'primary', disabled: true, action: () => handleAction('ReceiveFiles'), label: 'Received Shared Files' },
+        { iconType: <FileDownloadOutlinedIcon />, color: 'primary', disabled: isValid, action: () => handleAction('ReceiveFiles'), label: 'Received Shared Files' },
         { iconType: <CreateNewFolderOutlinedIcon />, color: 'secondary', disabled: isValid, action: () => handleAction('CreateFolder'), label: 'Create Folder' },
         { iconType: <UploadFileOutlinedIcon />, color: 'secondary', disabled: isValid, action: () => handleAction('UploadFileDetails'), label: 'Upload File' },
       ];
