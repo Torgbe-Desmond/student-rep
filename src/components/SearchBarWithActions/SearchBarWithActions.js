2d958cc73aafe91732cbb7d26c8e5326b8bd7912
Torgbe-Desmond <123torgbe@gmail.com>
2024-12-08 16:10:46 -0800

    ui changes

--- src/components/SearchBarWithActions/SearchBarWithActions.js
+++ src/components/SearchBarWithActions/SearchBarWithActions.js
@@ -11,18 +11,17 @@ import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
 import LogoutIcon from '@mui/icons-material/Logout';
 import './SearchBarWithActions.css';
 import ButtonIcon from '../Button/Button';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setSelectedFolders } from '../../Features/WorkSpace';
 import handleStack from '../HandleStack/HandleStack';
 import { useParams } from 'react-router-dom';
 
 function SearchBarWithActions({ folderData, setFilteredData, selectedItems, selectedFilesForOptions, selectedFoldersForOptions }) {
   const [searchTerm, setSearchTerm] = useState('');
-  const { directoryId } = useParams()
+  const { directoryId } = useParams();
   const dispatch = useDispatch();
   const [isValid, setIsValid] = useState(true);
   const [showSearch, setShowSearch] = useState(false);
-
 
   useEffect(() => {
     setIsValid(!directoryId);
@@ -44,11 +43,11 @@ function SearchBarWithActions({ folderData, setFilteredData, selectedItems, sele
   );
 
   const logout = [
-    { iconType: <LogoutIcon />, color: 'secondary', disabled: null , action: () => handleAction('Logout'), label: 'Logout' },
-  ]
+    { iconType: <LogoutIcon />, color: 'secondary', disabled: null, action: () => handleAction('Logout'), label: 'Logout' },
+  ];
 
   const buttonConfigs = [
-    { iconType: <DeleteIcon />, color: 'primary', disabled: selectedItems?.length === 0 || isValid , action: () => handleAction('Delete'), label: 'Delete' },
+    { iconType: <DeleteIcon />, color: 'primary', disabled: selectedItems?.length === 0 || isValid, action: () => handleAction('Delete'), label: 'Delete' },
     { iconType: <EditIcon />, color: 'primary', disabled: selectedFoldersForOptions?.length !== 1 || isValid, action: () => handleAction('RenameFolder'), label: 'Rename' },
     { iconType: <DriveFileMoveIcon />, color: 'primary', disabled: selectedItems?.length === 0 || isValid, action: () => handleAction('Move'), label: 'Move' },
     { iconType: <FileDownloadIcon />, color: 'primary', disabled: selectedFilesForOptions?.length !== 1 || isValid, action: () => handleAction('Download'), label: 'Download' },
@@ -58,17 +57,17 @@ function SearchBarWithActions({ folderData, setFilteredData, selectedItems, sele
     { iconType: <UploadFileOutlinedIcon />, color: 'secondary', disabled: isValid, action: () => handleAction('UploadFileDetails'), label: 'Upload File' },
   ];
 
-   // Handle scroll event
-   useEffect(() => {
+  // Handle scroll event
+  useEffect(() => {
     const handleScroll = () => {
       const totalHeight = document.documentElement.scrollHeight - window.innerHeight;
       const scrollPosition = window.scrollY || document.documentElement.scrollTop;
       const scrollPercentage = (scrollPosition / totalHeight) * 100;
 
-      // Show the search bar when scroll percentage is greater than 10%
+      // Show the search bar when scroll percentage is greater than 20%
       if (scrollPercentage > 20) {
         setShowSearch(true);
-      } else {
+      } else if(scrollPercentage < 1) {
         setShowSearch(false);
       }
     };
@@ -80,16 +79,15 @@ function SearchBarWithActions({ folderData, setFilteredData, selectedItems, sele
     };
   }, []);
 
-
   return (
     <div className="search-options">
-        {showSearch && (
-        <Box sx={{ position: "fixed", top: '5.6%', transform: "translateX(-50%)",transform: "translateY(-50%)", width: "90%", zIndex: 1000 }}>
+      {showSearch ? (
+        <Box sx={{ position: "fixed", top: '5.6%', transform: "translateX(-50%)", transform: "translateY(-50%)", width: "90%", zIndex: 1000 }}>
           <TextField
-            className="search-input"
+            className="search-input fixed"
             sx={{
-              boxShadow:' 0 0 10px rgba(63, 81, 181, 0.5)'
-             }}
+              boxShadow: '0 0 10px rgba(63, 81, 181, 0.5)'
+            }}
             label="Search files by name"
             variant="outlined"
             fullWidth
@@ -97,14 +95,16 @@ function SearchBarWithActions({ folderData, setFilteredData, selectedItems, sele
             onChange={(e) => setSearchTerm(e.target.value)}
           />
         </Box>
+      ) : (
+        <TextField
+          className="search-input"
+          label="Search files by name"
+          variant="outlined"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
       )}
-      <TextField
-        className="search-input"
-        label="Search files by name"
-        variant="outlined"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
+
       <div className="button-group">
         {buttonConfigs.map((config, index) => (
           <ButtonIcon
